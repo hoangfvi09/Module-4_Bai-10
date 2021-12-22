2930fa34687581d852ebadacf33216eb3679a994
hoangfvi09 <huonglt129@gmail.com>
2021-12-22 16:26:10 +0700

    luyen tap coookie

---
+++ spring-save-user-develop/src/main/java/com/codegym/controller/LoginController.java
@@ -0,0 +1,82 @@
+package com.codegym.controller;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import com.codegym.model.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@SessionAttributes("user")
+public class LoginController {
+
+    /*add user in model attribute*/
+    @ModelAttribute("user")
+    public User setUpUserForm() {
+        return new User();
+    }
+
+    @RequestMapping("/init")
+    public ModelAndView init(@CookieValue(value = "counter", defaultValue = "0") Long counter, HttpServletResponse response){
+        counter++;
+        Cookie cookie = new Cookie("counter", counter.toString());
+        cookie.setMaxAge(30);
+        response.addCookie(cookie); //luu cookie
+        ModelAndView modelAndView = new ModelAndView("/index");
+        modelAndView.addObject("cookie",cookie); // hien thi cookie
+        return modelAndView;
+    }
+
+    @RequestMapping("/login")
+    public String index(@CookieValue(value = "setUser", defaultValue = "") String setUser, Model model) {
+        Cookie cookie = new Cookie("setUser", setUser);
+        model.addAttribute("cookieValue", cookie);
+        return "/login";
+    }
+
+    @PostMapping("/dologin")
+    public String doLogin(@ModelAttribute("user") User user, Model model, @CookieValue(value = "setUser", defaultValue = "") String setUser,
+                          HttpServletResponse response, HttpServletRequest request) //value la name cua cookie
+    {
+        //implement business logic
+        if (user.getEmail().equals("admin@gmail.com") && user.getPassword().equals("12345")) {
+            if (user.getEmail() != null)
+                setUser = user.getEmail();
+
+            // create cookie and set it in response
+            Cookie cookie = new Cookie("setUser", setUser);
+            cookie.setMaxAge(24 * 60 * 60);
+            response.addCookie(cookie); // luu nguoi dung vua dang nhap vao cookie
+
+            //get all cookies
+            Cookie[] cookies = request.getCookies(); //lay tat ca cookie
+            //iterate each cookie
+            for (Cookie ck : cookies) {
+                //display only the cookie with the name 'setUser'
+                if (ck.getName().equals("setUser")) {
+                    model.addAttribute("cookieValue", ck);
+                    break;
+                } else {
+                    ck.setValue("");
+                    model.addAttribute("cookieValue", ck);
+                    break;
+                }
+            }
+            model.addAttribute("cookieValue", cookie);
+            model.addAttribute("message", "Login success. Welcome ");
+        } else {
+
+            setUser = user.getEmail();
+            Cookie cookie = new Cookie("setUser", setUser);
+            response.addCookie(cookie);
+            model.addAttribute("cookieValue", cookie);
+            model.addAttribute("message", "Login failed. Try again.");
+        }
+        return "/login";
+    }
+}
